d7f7b3cd4391a9eb5bf32c0c25b1a7df7929e269
Omar Mukadam <om@princeton.edu>
2016-09-15 21:42:52 -0400

    Added findAll for query: 'Select all items that occur within the next minute'

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ var connection = new Sequelize('demo', 'postgres', 'Sweileh95', {
     dialect: 'postgres'
 });
 
-/*
-var Article = connection.define('article', {
-    title: Sequelize.STRING,
-    body: Sequelize.TEXT
-});
-*/
 
 var Entity = connection.define('conference_transcription_db', {
     conference_time: {
@@ -22,19 +16,36 @@ var Entity = connection.define('conference_transcription_db', {
     }
 });
 
-connection.sync().then(function() {
+
+
+connection.sync({
+  force: true
+}).then(function() {
 
 var currentTime = new Date();
-/*
-var tenSecPlus = new Date();
-var fortySecPlus = new Date();
-var twoMinPlus = new Date();
- tenSecPlus = currentTime.setSeconds(currentTime.getSeconds + 10);
- fortySecPlus = currentTime.setSeconds(currentTime.getSeconds + 40);
- twoMinPlus = currentTime.setMinutes(currentTime.getMinutes + 2);
-*/
 
-  //Insert multiple items at once
+
+//Select all items that occur within the next minute
+Entity.findAll({
+  where: {
+    conference_time: {
+
+      //Greater than or equal to current time
+      $gte: new Date(currentTime.getTime()),
+
+      //less than 1 minute from now
+      $lt: new Date(currentTime.getTime() + 60*1000)
+    }
+  }
+}).then(function(entities){
+
+  //Display the result(s)
+  entities.forEach(entityLog)
+})
+
+
+/*
+  //Insert multiple items at once. For testing purposes
     Entity.bulkCreate(
       [
 
@@ -62,7 +73,36 @@ var twoMinPlus = new Date();
         meeting_subject: 'Conference call +2min'
       }
       ]
-    )
+    ).then(function()
+  {
+
+  //Select all items that occur within the next minute
+  Entity.findAll({
+    where: {
+      conference_time: {
+
+        //Greater than or equal to current time
+        $gte: new Date(currentTime.getTime()),
+
+        //less than 1 minute from now
+        $lt: new Date(currentTime.getTime() + 60*1000)
+      }
+    }
+  }).then(function(entities){
+
+    //Display the result(s)
+    entities.forEach(entityLog)
+  })
+
+  })
+*/
+
 }).catch(function(error) {
     console.log(error);
 });
+
+
+// Display properties of the entities objects
+var entityLog = function(currEntity) {
+  console.dir(currEntity.get());
+}
